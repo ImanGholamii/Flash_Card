cd1e4fdc55f3191913d6154048d5c87f059a1c43
Iman <imangholamiimi@gmail.com>
2024-09-09 20:50:06 +0330

    :zap: | UPDATE | Functions >> next_cards: flip_timer added to global variables | :zap: | UPDATE | window >>  window.after named flip_timer

--- main.py
+++ main.py
@@ -21,30 +21,29 @@ index = 0
 
 
 def next_card():
-    global index, words_list
-    canvas.itemconfig(bg_image, image=card_fg_img)
-    canvas.itemconfig(card_title, text=f"English")
+    global index, words_list, flip_timer
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(current_card_bg_image, image=card_fg_img)
+    canvas.itemconfig(card_title, text=f"English", fill="black")
     words = pandas.read_csv("data/english_words_with_translations.csv")
     words_list = words.to_dict("records")
     index = randint(1, len(words))
-    canvas.itemconfig(card_word, text=words_list[index]['English'])
+    canvas.itemconfig(card_word, text=words_list[index]['English'], fill="black")
+    flip_timer = window.after(3000, flip_card)
 
 
 def flip_card():
     """Show translate of Current Eng word"""
-    card_bg_img_adr = "images/card_back.png"
-    card_bg_img = PhotoImage(file=card_bg_img_adr)
-    canvas.create_image(400, 263, image=card_bg_img)
-    canvas.itemconfig(bg_image, image=card_bg_img)
-    canvas.itemconfig(card_title, text="فارسی")
-    canvas.itemconfig(card_word, text=words_list[index]['Persian'])
+    canvas.itemconfig(current_card_bg_image, image=card_bg_img)
+    canvas.itemconfig(card_title, text="فارسی", fill="white")
+    canvas.itemconfig(card_word, text=words_list[index]['Persian'], fill="white")
 
 
 # ---------------------------- window ------------------------------- #
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
-window.after(3000, flip_card)
+flip_timer = window.after(3000, flip_card)
 
 # ---------------------------- grid system ------------------------------- #
 for row in range(ROWS):
@@ -57,7 +56,12 @@ canvas = Canvas(width=800, height=526, highlightthickness=0)
 
 card_fg_img_adr = "images/card_front.png"
 card_fg_img = PhotoImage(file=card_fg_img_adr)
-bg_image = canvas.create_image(400, 263, image=card_fg_img)
+current_card_bg_image = canvas.create_image(400, 263, image=card_fg_img)
+
+card_bg_img_adr = "images/card_back.png"
+card_bg_img = PhotoImage(file=card_bg_img_adr)
+
+
 card_title = canvas.create_text(400, 150, text="", font=('Ariel', 40, 'italic'))
 card_word = canvas.create_text(400, 263, text="", font=('Ariel', 60, 'normal'))
 
